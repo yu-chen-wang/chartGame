bd0f85d40bc9fb4a3301e233491e64e72eaf8367
yu-chen-wang <yuwang0409@gmail.com>
2018-12-26 19:00:00 +0800

    遊戲頁 AI 已完成、缺 GameID 和 Player、製作登入頁(缺 playerID)

--- callApi.php
+++ callApi.php
@@ -1,8 +1,5 @@
 <?php
 
-// $url = 'http://192.168.10.148/CmMannaWCF/IsHoliday?date8=20181230&ck=936DA01F-9ABD-4d9d-80C7-02AF85C822A8';
-// $url = 'http://192.168.10.148/TickDbData/api/tick/get?time=2017/05/02';
-
 
 if(isset($_GET['method'])){
     $method = $_GET['method'];
@@ -15,6 +12,11 @@ else if(isset($_POST['method'])){
     $data = $_POST['data'];
     
 }
+
+// echo $method;
+// echo $url."<br>";
+// echo $data;
+// print_r($data);
 
 echo callAPI($method, $url, $data);
 
@@ -40,10 +42,10 @@ function callAPI($method, $url, $data){
  
     // OPTIONS:
     curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-       'APIKEY: 111111111111111111111',
-       'Content-Type: application/json',
-    ));
+   //  curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+   //     'APIKEY: 111111111111111111111',
+   //     'Content-Type: application/json',
+   //  ));
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
  
