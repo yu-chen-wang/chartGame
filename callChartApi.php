18fc4d8ea6c5ae47b9bed7dfbab38a7e0c1a1ca7
yu-chen-wang <yuwang0409@gmail.com>
2018-12-27 18:32:04 +0800

    遊戲頁缺 scatter、登入頁(缺註冊、忘記密碼)、大廰缺 tr onlick 事件

--- callChartApi.php
+++ callChartApi.php
@@ -1,6 +1,8 @@
 <?php
 
-// 用來 call http://www.cmoney.tw/MobileService/ashx/ 的 APi
+// 用來 call http://192.168.10.148/ChartGame 的 API
+// 注意: 在前端 ajax call 這頁 API 時，須把 object JSON.stringify() 後才丟
+
 
 if(isset($_GET['method'])){
     $method = $_GET['method'];
@@ -13,14 +15,7 @@ else {
     $data = $_POST['data'];
 }
 
-
-// echo $method;
-// echo $url."<br>";
-// echo $data;
-// print_r($data);
-
 echo callAPI($method, $url, $data);
-
 
 function callAPI($method, $url, $data){
     $curl = curl_init();
@@ -45,12 +40,14 @@ function callAPI($method, $url, $data){
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
- 
-   //  curl_setopt($curl, CURLOPT_HTTPHEADER, array(
-   //     'Content-Type: application/json',
-   //  ));
 
-    
+    curl_setopt_array($curl, array(
+        CURLOPT_HTTPHEADER => array(
+          "cache-control: no-cache",
+          "content-type: application/json",
+        ),
+    ));
+ 
     // EXECUTE:
     $result = curl_exec($curl);
 
